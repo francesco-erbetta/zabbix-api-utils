4eb6fdb8e9cbaec4f1a1e9ee7cb983a7ad3af42c
Luca Maranzano <liuk@linux.it>
2024-12-30 14:49:29 +0100

    Graph generator can take starttime and endtime via getopt

--- repgen/gg.sh
+++ repgen/gg.sh
@@ -4,9 +4,62 @@
 # Luca Maranzano <liuk@linux.it> - 2024
 #
 # This program should be rewritten in python for better performance.
-#set -x
+set -x
+
+usage() {
+    echo "Generate zabbix graphs via zapi utils."
+    echo "Usage: $0 [ -s starttime ] [ -t endtime ]"
+    echo "Default values: starttime=now-7d, endtime=now"
+    echo ""
+    echo "Example: $0 -s now-7d -t now"
+    exit 1
+}
+
+# Imposta le opzioni e specifica quali accettano un argomento (indicato con ':')
+OPTIONS="s:t:"
+LONGOPTS="starttime:,endtime:"
+
+# Analizza gli argomenti usando getopt
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+    echo "Getopt failed"
+    exit 1
+fi
+
+# Riorganizza gli argomenti secondo l'output di getopt
+eval set -- "$PARSED"
+
+# Variabili per memorizzare i valori degli argomenti
+starttime="now-7d"
+endtime="now"
+
+# Ciclo per analizzare gli argomenti
+while true; do
+    case "$1" in
+        -s|--starttime)
+            starttime="$2"
+            shift 2
+            ;;
+        -t|--endtime)
+            endtime="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Opzione non riconosciuta: $1" >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPDATA=$BASEDIR/repdata
+
+echo "Graph timeframe: from $starttime to $endtime"
 
 # Graph sizes
 GRW=900
@@ -38,7 +91,7 @@ generate_infofile() {
 }
 
 # Template Windows
-for h in $(../zthostfinder.py "Windows by Zabbix agent"); do
+for h in $(../zthostfinder.py "Windows by Zabbix agent" | sort ); do
   let counter=1
   echo Generating data for host: $h
   # Create data dir for this host
@@ -47,23 +100,23 @@ for h in $(../zthostfinder.py "Windows by Zabbix agent"); do
   
   current_graph="$REPDATA/$h/g$counter.png"; printf "\tGraph %s\n" $current_graph
   gid=$(../zhgraphfinder.py -e $h | grep "Windows: CPU utilization" | cut -d':' -f1)
-  ../zgetgraph.py -s now-7d -t now -W $GRW -H $GRH -f $current_graph $gid
+  ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $gid
   counter=$((counter+1))
   
   current_graph="$REPDATA/$h/g$counter.png"; printf "\tGraph %s\n" $current_graph
   gid=$(../zhgraphfinder.py -e $h | grep "Windows: Memory utilization" | cut -d':' -f1)
-  ../zgetgraph.py -s now-7d -t now -W $GRW -H $GRH -f $current_graph $gid
+  ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $gid
   counter=$((counter+1))
   
   # Loop for all disks/file systems
   for fsgrph in $(../zhgraphfinder.py -e $h | grep "Disk space usage (BVREP)" | cut -d':' -f1 | sort -n) ; do 
     current_graph="$REPDATA/$h/g$counter.png"; printf "\tGraph %s\n" $current_graph
-    ../zgetgraph.py -s now-7d -t now -W $GRW -H $GRH -f $current_graph $fsgrph
+    ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $fsgrph
     counter=$((counter+1))
   done
 done
 
-./r1.py
+./r1.py "Zabbix performances report" "ACME Corporation" "Last 7 days from today 30-12-2024"
 
 okular report.pdf &
 
