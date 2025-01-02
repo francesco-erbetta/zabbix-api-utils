9b96e046047d194a4fc036cf269f7da781f4de41
Luca Maranzano <liuk@linux.it>
2025-01-02 16:09:36 +0100

    gg.sh fix graph numbering, add linux template, repdata cleanup

--- repgen/gg.sh
+++ repgen/gg.sh
@@ -4,20 +4,25 @@
 # Luca Maranzano <liuk@linux.it> - 2024
 #
 # This program should be rewritten in python for better performance.
-# set -x
+
+# Bash safety
+set -e
+set -u
+set -o pipefail
+#set -x
 
 usage() {
     echo "Generate zabbix graphs via zapi utils."
-    echo "Usage: $0 [ -s starttime ] [ -t endtime ]"
-    echo "Default values: starttime=now-7d, endtime=now"
+    echo "Usage: $0 [ -s starttime ] [ -t endtime ] [ -o repdata ]" 
+    echo "Default values: starttime=now-7d, endtime=now, outdir=repdata"
     echo ""
-    echo "Example: $0 -s now-10d -t now"
+    echo "Example: $0 -s now-10d -t now --outdir myoutput"
     exit 1
 }
 
 # Imposta le opzioni e specifica quali accettano un argomento (indicato con ':')
-OPTIONS="s:t:"
-LONGOPTS="starttime:,endtime:"
+OPTIONS="s:t:o:"
+LONGOPTS="starttime:,endtime:,outdir:"
 
 # Analizza gli argomenti usando getopt
 PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
@@ -29,6 +34,7 @@ eval set -- "$PARSED"
 # Variabili per memorizzare i valori degli argomenti
 starttime="now-7d"
 endtime="now"
+outdir="repdata"
 
 # Ciclo per analizzare gli argomenti
 while true; do
@@ -39,6 +45,10 @@ while true; do
             ;;
         -t|--endtime)
             endtime="$2"
+            shift 2
+            ;;
+        -o|--outdir)
+            outdir="$2"
             shift 2
             ;;
         --)
@@ -54,19 +64,28 @@ while true; do
 done
 
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-REPDATA=$BASEDIR/repdata
+REPDATA=$BASEDIR/$outdir
 
 echo "Graph timeframe: from $starttime to $endtime"
+echo "repdata=$REPDATA"
 
 # Graph sizes
 GRW=900
 GRH=200
 
-cd $BASEDIR
+cd $BASEDIR || usage
+if [ "$(/bin/ls -A $REPDATA)" ]
+then
+  echo "Cleaning $REPDATA"
+  /bin/rm -fr $REPDATA/* 2>/dev/null
+else
+  echo "$REPDATA is empty or is missing (will be created)"
+fi
 
 # Create additional info file for a given host, they will be placed
 # in the top section of every page
-# Infofile generation is common for every kind of host
+# Infofile generation is common for every host, at least if you are using 
+# Windows and Linux templates, beware YMMV
 # Use HTML tags for text formatting
 generate_infofile() {
   h=$1
@@ -87,33 +106,77 @@ generate_infofile() {
   printf "Host groups: <b>%s</b><br>\n" "$group_list" >>$info_file
 }
 
+# NOTE: Use "zhgraphfinder.py -e YOURHOST" to find your suitable patterns 
+# to get the graphs that you need
+
 # Template Windows
-for h in $(../zthostfinder.py "Windows by Zabbix agent" | sort ); do
+let hcounter=1
+total=$(../zthostfinder.py -m "Windows by Zabbix agent" | wc -l)
+IFS=$'\n'; for h in $(../zthostfinder.py -m "Windows by Zabbix agent" | sort ); do
   let counter=1
-  echo Generating data for host: $h
+  printf "Generating data for host: %s (%s/%s)\n" $h $hcounter $total
   # Create data dir for this host
   if [ ! -d "$REPDATA/$h" ]; then mkdir -p "$REPDATA/$h"; fi
   generate_infofile "$h"
   
-  current_graph="$REPDATA/$h/g$counter.png"; printf "\tGraph %s\n" $current_graph
+  printf -v current_graph "%s/%s/g%03d.png" $REPDATA $h $counter
+  printf "\tGraph %s\n" $current_graph
   gid=$(../zhgraphfinder.py -e $h | grep "Windows: CPU utilization" | cut -d':' -f1)
   ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $gid
   counter=$((counter+1))
   
-  current_graph="$REPDATA/$h/g$counter.png"; printf "\tGraph %s\n" $current_graph
+  printf -v current_graph "%s/%s/g%03d.png" $REPDATA $h $counter
+  printf "\tGraph %s\n" $current_graph
   gid=$(../zhgraphfinder.py -e $h | grep "Windows: Memory utilization" | cut -d':' -f1)
   ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $gid
   counter=$((counter+1))
   
   # Loop for all disks/file systems
   for fsgrph in $(../zhgraphfinder.py -e $h | grep "Disk space usage (BVREP)" | cut -d':' -f1 | sort -n) ; do 
-    current_graph="$REPDATA/$h/g$counter.png"; printf "\tGraph %s\n" $current_graph
+    printf -v current_graph "%s/%s/g%03d.png" $REPDATA $h $counter
+    printf "\tGraph %s\n" $current_graph
     ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $fsgrph
     counter=$((counter+1))
   done
+  hcounter=$((hcounter+1))
 done
 
-./r1.py "Zabbix performances report" "ACME Corporation" "Last 7 days from today 30-12-2024"
+# Template Linux
+let hcounter=1
+total=$(../zthostfinder.py -m "Linux by Zabbix agent" | wc -l)
+for h in $(../zthostfinder.py -m "Linux by Zabbix agent" | sort ); do
+  let counter=1
+  printf "Generating data for host: %s (%s/%s)\n" $h $hcounter $total
+  # Create data dir for this host
+  if [ ! -d "$REPDATA/$h" ]; then mkdir -p "$REPDATA/$h"; fi
+  generate_infofile "$h"
+  
+  printf -v current_graph "%s/%s/g%03d.png" $REPDATA $h $counter
+  printf "\tGraph %s\n" $current_graph
+  gid=$(../zhgraphfinder.py -e $h | grep "Linux: CPU utilization" | cut -d':' -f1)
+  ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $gid
+  counter=$((counter+1))
+  
+  printf -v current_graph "%s/%s/g%03d.png" $REPDATA $h $counter
+  printf "\tGraph %s\n" $current_graph
+  gid=$(../zhgraphfinder.py -e $h | grep "Linux: Memory usage" | cut -d':' -f1)
+  ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $gid
+  counter=$((counter+1))
+  
+  # Loop for all disks/file systems
+  for fsgrph in $(../zhgraphfinder.py -e $h | grep "Disk space usage (BVREP)" | cut -d':' -f1 | sort -n) ; do 
+    printf -v current_graph "%s/%s/g%03d.png" $REPDATA $h $counter
+    printf "\tGraph %s\n" $current_graph
+    ../zgetgraph.py -s "$starttime" -t "$endtime" -W $GRW -H $GRH -f $current_graph $fsgrph
+    counter=$((counter+1))
+  done
+  hcounter=$((hcounter+1))
+done
+
+unset IFS
+
+tstamp=$(date +%Y-%m-%d-H%H%M)
+./r1.py "Infrastructure report by Zabbix" "ACME Corporation" "Last 7 days from $tstamp"
 
 okular report.pdf &
 
