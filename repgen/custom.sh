f9666bdfb8273caa369dc540e9281adfc1ddac66
francesco-erbetta <francesco-erbetta@port-erbetta>
2025-01-10 15:03:11 +0100

    Tweaking custom.sh

--- repgen/custom.sh
+++ repgen/custom.sh
@@ -19,10 +19,11 @@ echo -n "Inserisci la url del frontend zabbix (es. https://zabbix.cliente.local/
 read zurl
 echo -n "Inserisci la mail alla quale si intende inviare il report"
 read mailcliente
+echo -n "Inserisci la frase di congedo"
+read endsentence
+sed -i "s/[placeholder]/$endsentence/g" $WORKDIR/repgen/gg.sh
 
-echo "[Zabbix API]
-username=$zusr
-password=$zpwd
+echo "[Zabbix API] username=$zusr password=$zpwd
 api=$zurl
 no_verify=true" > /home/zabbix-reports/.zabbix-api.conf
 
@@ -33,11 +34,11 @@ while true; do
     read -p "Inserisci 1 o 2: " choice
 
     if [[ "$choice" == "1" ]]; then
-        echo "0 8 * * 1 $WORKDIR/repgen/gg.sh -s now-7d -t now && echo "Testo mail" | mail -s "Report settimanale $cliente" -a $WORKDIR/repgen/report.pdf $mailcliente" | crontab -
+        echo "0 8 * * 1 source $WORKDIR/zabbix-reports-venv/bin/activate; $WORKDIR/repgen/gg.sh -s now-1w -t now; cat "$WORKDIR/repgen/mailtext.txt" | mail -s "Report settimanale performances infrastruttura" -a $WORKDIR/repgen/report.pdf $mailcliente" | crontab -
         break
     elif [[ "$choice" == "2" ]]; then
-        sed -i 's/Last 7 days/Last 30 days/g' $WORKDIR/repgen/gg.sh
-        echo "0 0 1 * * $WORKDIR/repgen/gg.sh -s now-30d -t now && echo "Testo mail" | mail -s "Report mensile $cliente" -a $WORKDIR/repgen/report.pdf $mailcliente" | crontab -
+        sed -i 's/Last 7 days/Last month/g' $WORKDIR/repgen/gg.sh
+        echo "0 0 1 * * source $WORKDIR/zabbix-reports-venv/bin/activate; $WORKDIR/repgen/gg.sh -s now-1M -t now; cat "$WORKDIR/repgen/mailtext.txt" | mail -s "Report mensile performances infrastrutturale" -a $WORKDIR/repgen/report.pdf $mailcliente" | crontab -
         break
     else
         echo "Input invalido. Inserisci 1 o 2."
