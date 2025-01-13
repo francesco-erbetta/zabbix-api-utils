99cea63be1f3f3a4da2e87071e738bd970496db5
francesco-erbetta <francesco-erbetta@port-erbetta>
2025-01-13 08:22:53 +0100

    fix gg.sh

--- repgen/gg.sh
+++ repgen/gg.sh
@@ -176,7 +176,7 @@ done
 unset IFS
 
 tstamp=$(date +%Y-%m-%d-H%H%M)
-./r1.py "Infrastructure report by Zabbix" "ACME Corporation" "Last 7 days from $tstamp"
+/home/zabbix-reports/r1.py "Infrastructure report by Zabbix" "ACME Corporation" "Last 7 days from $tstamp"
 
 #Generazione testo mail
 #if [[ $2 == now-1M ]] ; then
