9a0bc4f0b582e990d633b54e52da1fd9d1035a35
francesco-erbetta <francesco@francescoerbetta.cloud>
2025-02-04 16:52:15 +0100

    Aggiunta generazione automaica password zabbix-reports

--- repgen/setup.sh
+++ repgen/setup.sh
@@ -22,14 +22,14 @@ if [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]; then
   done
 elif [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "almalinux" ] || [ "$DISTRO" == "rocky" ]; then
   for pkg in python3 python3-pip python3-venv sendmail; do
-    if ! rpm -q "$pkg" > /dev/null 2>&1; then
+    if ! rpm -q "$pkg" >/dev/null 2>&1; then
       yum install -y "$pkg"
     fi
   done
 elif [ "$DISTRO" == "sles" ] || [ "$DISTRO" == "opensuse" ]; then
   zypper refresh
   for pkg in python3 python3-pip python3-venv sendmail; do
-    if ! rpm -q "$pkg" > /dev/null 2>&1; then
+    if ! rpm -q "$pkg" >/dev/null 2>&1; then
       zypper install -y "$pkg"
     fi
   done
@@ -40,9 +40,11 @@ fi
 
 #Crea nuovo utente per generazione report
 useradd -m -s /bin/bash zabbix-reports
-echo -n "Scegli una password per l'utente zabbix-reports
-"
-passwd zabbix-reports
+zabbpwd=$(cat /dev/urandom | tr -dc A-Za-z0-9 | fold -w 12 | head -n 1)
+echo "zebbix-reports:$zabbpwd" | chpasswd
+echo "La password per l'utente locale zabbix-reports è $zabbpwd"
+echo "Assicurati che sia salvata in modo sicuro e premi qualunque tasto per continuare..."
+read
 cp -r ../../zabbix-api-utils /home/zabbix-reports
 chown -R zabbix-reports:zabbix-reports /home/zabbix-reports/zabbix-api-utils
 su zabbix-reports /home/zabbix-reports/zabbix-api-utils/repgen/custom.sh
