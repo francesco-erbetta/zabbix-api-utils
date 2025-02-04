25c4853350acb3256d135b486926d6452f707196
francesco-erbetta <francesco@francescoerbetta.cloud>
2025-02-04 16:24:36 +0100

    Fix missing TO: variable replacement

--- repgen/custom.sh
+++ repgen/custom.sh
@@ -4,7 +4,7 @@ WORKDIR=/home/zabbix-reports/zabbix-api-utils
 mkdir $WORKDIR/zabbix-reports-venv
 python3 -m venv $WORKDIR/zabbix-reports-venv
 source $WORKDIR/zabbix-reports-venv/bin/activate &&
-pip install -r $WORKDIR/requirements.txt
+  pip install -r $WORKDIR/requirements.txt
 
 #Customizzazione cliente
 echo -n "Inserisci nome cliente: "
@@ -23,6 +23,7 @@ read -e mittente
 sed -i "/^FROM=/s/\".*\"/\"$mittente\"/" $WORKDIR/repgen/sendmail.sh
 echo -n "Inserisci una o più mail (separate da virgola) alle quali si intende inviare il report: "
 read -e mailcliente
+sed -i "/^TO=/s/\".*\"/\"$mailcliente\"/" $WORKDIR/repgen/sendmail.sh
 echo -n "Inserisci una o più mail (separate da virgola) da mettere in copia: "
 read -e mailcopia
 sed -i "/^CC=/s/\".*\"/\"$mailcopia\"/" $WORKDIR/repgen/sendmail.sh
@@ -34,23 +35,23 @@ echo "[Zabbix API]
 username=$zusr 
 password=$zpwd
 api=$zurl
-no_verify=true" > /home/zabbix-reports/.zabbix-api.conf
+no_verify=true" >/home/zabbix-reports/.zabbix-api.conf
 
 while true; do
-    echo "A che cadenza devono essere generati i report?"
-    echo "1) Settimanale"
-    echo "2) Mensile"
-    read -p "Inserisci 1 o 2: " choice
+  echo "A che cadenza devono essere generati i report?"
+  echo "1) Settimanale"
+  echo "2) Mensile"
+  read -p "Inserisci 1 o 2: " choice
 
-    if [[ "$choice" == "1" ]]; then
-	sed -i "s/\[cadenza\]/settimanale/g" $WORKDIR/repgen/sendmail.sh
-        echo "0 8 * * 1 source $WORKDIR/zabbix-reports-venv/bin/activate; $WORKDIR/repgen/gg.sh -s now-1w -t now; $WORKDIR/repgen/sendmail.sh" | crontab -
-        break
-    elif [[ "$choice" == "2" ]]; then
-	sed -i "s/\[cadenza\]/mensile/g" $WORKDIR/repgen/sendmail.sh
-        echo "0 0 1 * * source $WORKDIR/zabbix-reports-venv/bin/activate; $WORKDIR/repgen/gg.sh -s now-1M -t now; $WORKDIR/repgen/sendmail.sh" | crontab -
-        break
-    else
-        echo "Input invalido. Inserisci 1 o 2."
-    fi
+  if [[ "$choice" == "1" ]]; then
+    sed -i "s/\[cadenza\]/settimanale/g" $WORKDIR/repgen/sendmail.sh
+    echo "0 8 * * 1 source $WORKDIR/zabbix-reports-venv/bin/activate; $WORKDIR/repgen/gg.sh -s now-1w -t now; $WORKDIR/repgen/sendmail.sh" | crontab -
+    break
+  elif [[ "$choice" == "2" ]]; then
+    sed -i "s/\[cadenza\]/mensile/g" $WORKDIR/repgen/sendmail.sh
+    echo "0 0 1 * * source $WORKDIR/zabbix-reports-venv/bin/activate; $WORKDIR/repgen/gg.sh -s now-1M -t now; $WORKDIR/repgen/sendmail.sh" | crontab -
+    break
+  else
+    echo "Input invalido. Inserisci 1 o 2."
+  fi
 done
