18ea14ab062f8f3f2327c18a6b4d30a329f5999e
francesco-erbetta <francesco@francescoerbetta.cloud>
2025-03-03 13:29:03 +0100

    source mancante su prima linea cronscript.sh

--- repgen/custom.sh
+++ repgen/custom.sh
@@ -45,13 +45,13 @@ while true; do
 
   if [[ "$choice" == "1" ]]; then
     sed -i "s/\[cadenza\]/settimanale/g" $WORKDIR/repgen/sendmail.sh
-    echo -e "#!/bin/bash\n$WORKDIR/zabbix-reports-venv/bin/activate;\n$WORKDIR/repgen/gg.sh -s now-1w -t now;\n$WORKDIR/repgen/sendmail.sh" >$WORKDIR/repgen/cronscript.sh
+    echo -e "#!/bin/bash\nsource $WORKDIR/zabbix-reports-venv/bin/activate;\n$WORKDIR/repgen/gg.sh -s now-1w -t now;\n$WORKDIR/repgen/sendmail.sh" >$WORKDIR/repgen/cronscript.sh
     chmod +x $WORKDIR/repgen/cronscript.sh
     echo "0 8 * * 1 $WORKDIR/repgen/cronscript.sh" | crontab -
     break
   elif [[ "$choice" == "2" ]]; then
     sed -i "s/\[cadenza\]/mensile/g" $WORKDIR/repgen/sendmail.sh
-    echo -e "#!/bin/bash\n$WORKDIR/zabbix-reports-venv/bin/activate;\n$WORKDIR/repgen/gg.sh -s now-1M -t now;\n$WORKDIR/repgen/sendmail.sh" >$WORKDIR/repgen/cronscript.sh
+    echo -e "#!/bin/bash\nsource $WORKDIR/zabbix-reports-venv/bin/activate;\n$WORKDIR/repgen/gg.sh -s now-1M -t now;\n$WORKDIR/repgen/sendmail.sh" >$WORKDIR/repgen/cronscript.sh
     chmod +x $WORKDIR/repgen/cronscript.sh
     echo "0 0 1 * * $WORKDIR/repgen/cronscript.sh" | crontab -
     break
